f7f9bd9cf72ed227bd4a80d00314c8ad0b63a05c
NIPUNMADHUSANKA <nipunmadhusanka201080@gmail.com>
2026-01-07 23:58:04 +0530

    add JWT token to .env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
-import { JWT_SECRET } from 'src/configs/jwt-secret';
+import "dotenv/config";
 import { PassportModule } from '@nestjs/passport';
 import { PassportAuthController } from './passport-auth.controller';
 import { LocalStrategy } from './strategies/local.strategy';
@@ -17,7 +17,7 @@ import { DatabaseModule } from 'src/database/database.module';
     PassportModule,
     JwtModule.register({
       global: true,
-      secret: JWT_SECRET,
+      secret: process.env.JWT_SECRET,
       signOptions: {expiresIn: '1d'},
     })
   ],
